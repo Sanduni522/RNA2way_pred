5bcbcbade0f81aa2c685e7a224087564b08bd65c
Sanduni Deenalattha <78231679+Sanduni522@users.noreply.github.com>
2024-04-21 23:04:46 -0500

    Update cli.py

--- RNA2way_pred/cli.py
+++ RNA2way_pred/cli.py
@@ -13,10 +13,8 @@ from tensorflow.keras.models import load_model
 import math
 from math import log
 from keras.callbacks import EarlyStopping
-from . import pdb_seq
 import subprocess
 from xlwt import Workbook
-from . import build_pdb_set
 import xlrd
 import openpyxl
 from biopandas.pdb import PandasPdb
